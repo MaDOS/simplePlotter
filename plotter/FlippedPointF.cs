bcb580775c2cdcbf148f71fe37c46ded923e7240
l00s3s <rbechtel.ostmann@googlemail.com>
2014-03-19 22:24:43 +0100

    some bugs still unfixed but if you don't missuse it it works like a charm

---
+++ plotter/FlippedPointF.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+
+public class FlippedPointF
+{
+    public float X { get; set; }
+    public float Y { get; set; }
+
+    public FlippedPointF(float x, float y)
+    { 
+        this.X = x;
+        this.Y = y; 
+    }
+
+    public PointF toPoint()
+    {
+        return new PointF(this.X, -this.Y);
+    }
+}
